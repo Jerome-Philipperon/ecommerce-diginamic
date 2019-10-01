70fc7a4d5e94bca6c00108be010bdfd97cec3206
Jerome <jerome.philipperon2@gmail.com>
2019-10-01 15:25:23 +0200

    Use article and list

--- src/app/home/list/article/article.component.ts
+++ src/app/home/list/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import Article from '../../../Models/article';
 
 @Component({
   selector: 'app-article',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  @Input() article : Article;
 
   constructor() { }
 
--- src/app/home/list/list.component.ts
+++ src/app/home/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Article from '../../Models/article';
 
 @Component({
   selector: 'app-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  articleList : Article[] = [
+    new Article ('canard', 'canard jaune en plastique', 5, 20),
+    new Article ('chat', 'peluche de chat', 20, 30),
+  ]
 
   constructor() { }
 
